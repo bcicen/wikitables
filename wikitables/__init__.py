50cafabdeafd26891b5be6bdda92251c100c1eb0
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-06-19 22:51:18 -0400

    lower default logging level

--- wikitables/__init__.py
+++ wikitables/__init__.py
@@ -4,17 +4,15 @@ import mwparserfromhell as mwp
 from wikitables.client import Client
 from wikitables.models import WikiTable, ftag
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARN)
 log = logging.getLogger('wikitables')
-
-mtables = lambda node: node.tag == "table"
 
 def import_tables(article):
     client = Client()
     page = client.fetch_page(article)
     body = page['revisions'][0]['*']
 
-    ## parse nodes for tables
+    ## parse for tables
     raw_tables = mwp.parse(body).filter_tags(matches=ftag('table'))
 
     def _table_gen():
