c642ecaf6f96bd7ad2b1db56559d2f13a59aef19
Knut Hühne <knut@k-nut.eu>
2019-02-04 20:25:10 +0100

    add test for change-template
    see https://en.wikipedia.org/wiki/Template:Change

--- test.py
+++ test.py
@@ -25,6 +25,34 @@ class TestWikiTables(unittest.TestCase):
         self.assertEqual(len(table.rows), 2)
         self.assertEqual(list(table.rows[0].keys()), ['Column 1 header', 'Column 2 header'])
 
+    def test_complex_table(self):
+        source = """
+{| class="wikitable sortable"
+! 2018<br>rank 
+! [[Municipalities of Brazil|City]]
+! [[States of Brazil|State]]
+! 2018<br>Estimate
+! 2010<br>Census
+! Change
+|-
+! 1
+|'''''[[São Paulo]]'''''
+| {{flag|São Paulo}}
+| {{change|invert=on|12176866|10659386}} 
+|-
+! 2
+| '''''[[Rio de Janeiro]]'''''
+| {{flag|Rio de Janeiro}}
+| {{change|invert=on|6688927|5940224}}
+|}
+"""
+        raw_tables = mwp.parse(source).filter_tags(matches=ftag('table'))
+
+        table = WikiTable("Test Table 2", raw_tables[0])
+
+        self.assertEqual(len(table.rows), 2)
+        self.assertEqual(list(table.rows[0].keys()), ['2018rank', 'City', 'State', '2018Estimate', '2010Census', 'Change'])
+
 
 if __name__ == '__main__':
     unittest.main()
